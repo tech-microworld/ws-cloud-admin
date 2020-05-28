5bd393926d950097d4aa039523e56f976721c984
FengJianxin <fengjianxin2012@gmail.com>
2020-05-28 20:03:50 +0800

    route: 支持协议类型修改

--- src/views/gateway/routes/index.vue
+++ src/views/gateway/routes/index.vue
@@ -67,7 +67,14 @@
         style="width: 400px margin-left:50px"
       >
         <el-form-item label="协议" prop="protocol">
-          <el-input v-model="dataFormModel.protocol" placeholder="eg: http" />
+          <el-select v-model="dataFormModel.protocol" placeholder="选择协议">
+            <el-option
+              v-for="item in protocolOptions"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            />
+          </el-select>
         </el-form-item>
         <el-form-item label="路由前缀" prop="prefix">
           <el-input v-model="dataFormModel.prefix" placeholder="eg: /openapi/user" />
@@ -178,7 +185,17 @@ export default {
           { required: true, message: '请输入服务名', trigger: 'blur' }
         ],
         propsData: [{ validator: validatePropsData, trigger: 'blur' }]
-      }
+      },
+      protocolOptions: [
+        {
+          value: 'http',
+          label: 'http'
+        },
+        {
+          value: 'grpc',
+          label: 'grpc'
+        }
+      ]
     }
   },
   created() {
